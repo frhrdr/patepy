39762914b018c5bd382ba31361ed564f2545d2af
frhrdr <frederikharder@gmail.com>
2019-07-15 16:38:40 +0200

    raw first draft of the pate accountant setup

---
+++ pate-source-edited/aliases.py
@@ -0,0 +1,92 @@
+from pate_core import compute_logq_gaussian, rdp_data_independent_gaussian, rdp_gaussian, _log1mexp, \
+  compute_eps_from_delta, compute_logpr_answered
+from smooth_sensitivity import compute_local_sensitivity_bounds_gnmax, compute_local_sensitivity_bounds_threshold, \
+  compute_rdp_of_smooth_sensitivity_gaussian, compute_discounted_max
+
+
+# define aliases of functions from pate_core and smooth_sensitivity in order to simplify things a little bit.
+
+def get_log_q(votes, sigma):
+  """
+
+  :param votes:
+  :param sigma:
+  :return:
+  """
+  return compute_logq_gaussian(votes, sigma)
+
+
+def rdp_max_vote(sigma, orders, log_q=None):
+  """
+
+  :param sigma:
+  :param orders:
+  :param log_q:
+  :return:
+  """
+  if log_q is None:
+    return rdp_data_independent_gaussian(sigma, orders)
+  else:
+    return rdp_gaussian(log_q, sigma, orders)
+
+
+def rdp_threshold(sigma, orders, threshold, votes):
+  """
+
+  :param sigma:
+  :param orders:
+  :param threshold:
+  :param votes:
+  :return:
+  """
+  log_pr_answered = compute_logpr_answered(threshold, sigma, votes)
+  logq = min(log_pr_answered, _log1mexp(log_pr_answered)) if log_pr_answered is not None else None
+  return rdp_max_vote(2**.5 * sigma, orders, logq)
+
+
+def rdp_to_dp(orders, rdp, delta):
+  """
+
+  :param orders:
+  :param rdp:
+  :param delta:
+  :return:
+  """
+  return compute_eps_from_delta(orders, rdp, delta)
+
+
+def local_sensitivity(votes, num_teachers, sigma, order, thresh=None):
+  """
+
+  :param votes:
+  :param num_teachers:
+  :param sigma:
+  :param order:
+  :param thresh:
+  :return:
+  """
+  if thresh is not None:
+    return compute_local_sensitivity_bounds_threshold(votes, num_teachers, thresh, sigma, order)
+  else:
+    return compute_local_sensitivity_bounds_gnmax(votes, num_teachers, sigma, order)
+
+
+def local_to_smooth_sens(beta, ls_by_dist):
+  """
+
+  :param beta:
+  :param ls_by_dist:
+  :return:
+  """
+  return compute_discounted_max(beta, ls_by_dist)
+
+
+def rdp_eps_release(beta, sigma, order):
+  """
+
+  :param beta:
+  :param sigma:
+  :param order:
+  :return:
+  """
+  return compute_rdp_of_smooth_sensitivity_gaussian(beta, sigma, order)
--- pate-source-edited/pate_accountant.py
+++ pate-source-edited/pate_accountant.py
@@ -1,49 +1,8 @@
 import numpy as np
 import torch as pt
-from pate_core import compute_logq_gaussian, rdp_data_independent_gaussian, rdp_gaussian, _log1mexp, \
-  compute_eps_from_delta, compute_logpr_answered
-from smooth_sensitivity import compute_local_sensitivity_bounds_gnmax, compute_local_sensitivity_bounds_threshold, \
-  compute_rdp_of_smooth_sensitivity_gaussian, compute_discounted_max
+from aliases import get_log_q, rdp_max_vote, rdp_threshold, rdp_to_dp, \
+  local_sensitivity, local_to_smooth_sens, rdp_eps_release
 
-# DEFINE ALIASES
-
-
-def get_log_q(counts, sigma):
-  return compute_logq_gaussian(counts, sigma)
-
-
-def rdp_max_vote(sigma, orders, log_q=None):
-  if log_q is None:
-    return rdp_data_independent_gaussian(sigma, orders)
-  else:
-    return rdp_gaussian(log_q, sigma, orders)
-
-
-def rdp_threshold(sigma, orders, log_pr_answered=None):
-  logq = min(log_pr_answered, _log1mexp(log_pr_answered)) if log_pr_answered is not None else None
-  return rdp_max_vote(2**.5 * sigma, orders, logq)
-
-
-def rdp_to_dp(orders, rdp, delta):
-  return compute_eps_from_delta(orders, rdp, delta)
-
-
-def local_sensitivity(counts, num_teachers, sigma, order, thresh=None):
-  if thresh is not None:
-    return compute_local_sensitivity_bounds_threshold(counts, num_teachers, thresh, sigma, order)
-  else:
-    return compute_local_sensitivity_bounds_gnmax(counts, num_teachers, sigma, order)
-
-
-def local_to_smooth_sens(beta, ls_by_dist):
-  return compute_discounted_max(beta, ls_by_dist)
-
-
-def rdp_eps_release(beta, sigma, order):
-  return compute_rdp_of_smooth_sensitivity_gaussian(beta, sigma, order)
-
-
-# DEFINE CLASS TO TRACK PRIVACY LOSS AND EMPLOYS GNMAX GN-conf and CN-interactive training
 
 class PATEPyTorch:
   """
@@ -52,149 +11,126 @@ class PATEPyTorch:
   privacy accounting is done in numpy, for vote perturbation, pytorch is assumed
   - track privacy loss during training
   - determine, which teacher responses are used for training following GNMax, GN-conf, GN-int or a mix of the latter two
-  - terminate training when a predetermined privacy budget is exhausted
+  (- terminate training when a predetermined privacy budget is exhausted) useful budget definition is hard to define
   - log  training details
   """
 
-  def __init__(self, target_delta, sigma_vote, n_teachers, sigma_eps_release,
-               threshold_mode=None, threshold_t=None, threshold_gamma=None, sigma_thresh=None,
+  def __init__(self, target_delta, sigma_votes, n_teachers, sigma_eps_release,
+               threshold_mode='basic', threshold_t=None, threshold_gamma=None, sigma_thresh=None,
                short_list_orders=True):
     super(PATEPyTorch, self).__init__()
 
+    self.n_teachers = n_teachers
+    self.orders = self._get_orders(short_list_orders)
     self.target_delta = target_delta
-    self.budget_spent = 0.
-    self.sigma_vote = sigma_vote
+    self.sigma_votes = sigma_votes
     self.sigma_thresh = sigma_thresh
     self.sigma_eps_release = sigma_eps_release
+
     self.threshold_mode = None
-    self.set_threshold_mode(threshold_mode)
+    self.basic_mode = 0
+    self.confident_mode = 1
+    self.interactive_mode = 2
+    self.threshold_mode = self._get_threshold_mode(threshold_mode)
+
     self.threshold_T = threshold_t
     self.threshold_gamma = threshold_gamma
-    self.orders = self.get_orders(short_list_orders)
-    self.rdp_eps_by_order = np.zeros(len(self.orders))
-    self.vote_log = []
-    self.n_teachers = n_teachers
+    self.rdp_eps_by_order = np.zeros(len(self.orders))  # stores the data-dependent privacy loss
+    self.votes_log = []  # stores votes and whether the threshold was passsed for smooth sensitivity analysis
     self.selected_order = None
     self.data_dependent_eps = None
-    # self.n_labels = None
-
-  def set_threshold_mode(self, threshold_mode):
-    assert threshold_mode in (None, 'conf', 'int')
-    self.threshold_mode = threshold_mode
 
   @staticmethod
-  def get_orders(short_list):
+  def _get_orders(short_list):
     if short_list:
       return np.round(np.concatenate((np.arange(2, 50 + 1, 1), np.logspace(np.log10(50), np.log10(1000), num=20))))
     else:
       return np.concatenate((np.arange(2, 100 + 1, .5), np.logspace(np.log10(100), np.log10(500), num=100)))
 
-  def add_rdp_loss(self, counts, sigma, thresh=False):
+  def _get_threshold_mode(self, threshold_mode):
+    modes = {'basic': self.basic_mode, 'confident': self.confident_mode, 'interactive': self.interactive_mode}
+    assert threshold_mode in modes
+    return modes[threshold_mode]
+
+  def _add_rdp_loss(self, votes, sigma, thresh=False):
     """
-    calculates privacy loss from counts and the sigma that was used to perturb them
-    :param counts:
+    calculates privacy loss from votes and the sigma that was used to perturb them
+    :param votes:
     :param sigma:
     :param thresh:
     :return:
     """
     if thresh:
-      log_pr_answered = compute_logpr_answered(self.threshold_T, sigma, counts)
-      rdp_thresh = rdp_threshold(sigma, self.orders, log_pr_answered)
-      self.rdp_eps_by_order += rdp_thresh
+      self.rdp_eps_by_order += rdp_threshold(sigma, self.orders, self.threshold_T, votes)
     else:
-      log_q = get_log_q(counts, sigma)
-      rdp_query = rdp_max_vote(sigma, self.orders, log_q)
-      self.rdp_eps_by_order += rdp_query
+      self.rdp_eps_by_order += rdp_max_vote(sigma, self.orders, get_log_q(votes, sigma))
 
-  def rdp_max_vote(self, counts, preds):
+  def gn_max(self, votes, preds):
     """
     depending on threshold_mode, return index of noisy teacher consensus, confident student label or None
-    :param counts: teacher votes
+    :param votes: teacher votes
     :param preds: student predictions (vector of class probabilities)
     :return:
     """
     release_votes = False
     data_intependent_ret = None
+    thresh_votes = votes
 
-    if self.threshold_mode is None:
+    # thresholding based on mode
+    if self.threshold_mode == self.basic_mode:
       release_votes = True
 
-    elif self.threshold_mode is 'conf':
-      self.add_rdp_loss(counts, self.sigma_thresh, thresh=True)
-      if pt.max(counts) + pt.normal(0., self.sigma_thresh) < self.threshold_T:
+    elif self.threshold_mode == self.confident_mode:
+      self._add_rdp_loss(votes, self.sigma_thresh, thresh=True)
+      if pt.max(votes) + pt.normal(0., self.sigma_thresh) >= self.threshold_T:
         release_votes = True
 
-    elif self.threshold_mode is 'int':
-      self.add_rdp_loss(counts, self.sigma_thresh, thresh=True)  # TODO verify this is correct. very likely different.
-      if pt.max(counts - self.n_teachers * preds) + pt.normal(0., self.sigma_thresh) >= self.threshold_T:
+    elif self.threshold_mode == self.interactive_mode:
+      thresh_votes = votes - self.n_teachers * preds
+      self._add_rdp_loss(thresh_votes, self.sigma_thresh, thresh=True)
+      if pt.max(thresh_votes) + pt.normal(0., self.sigma_thresh) >= self.threshold_T:
         release_votes = True
       elif pt.max(preds) > self.threshold_gamma:
-        # self.check_budget()
         data_intependent_ret = pt.argmax(preds)
 
-    self.vote_log.append((counts, release_votes))
+    self.votes_log.append((votes, thresh_votes, release_votes))
 
+    # release max vote if threshold is passed
     if release_votes:
-      self.add_rdp_loss(counts, self.sigma_vote, thresh=False)
-      # self.check_budget()
-      return pt.argmax(counts + pt.normal(pt.ones_like(counts), self.sigma_thresh))
+      self._add_rdp_loss(votes, self.sigma_votes, thresh=False)
+      return pt.argmax(votes + pt.normal(pt.ones_like(votes), self.sigma_thresh))
     else:
       return data_intependent_ret
 
-  def data_dependent_delta_dp(self):
+  def gn_max_batched(self, votes, preds):
+    """
+    cuts batch of votes and preds into samples and computes rdp_max_vote individually, then aggregates the results.
+    Filters out none values
+    more efficient version requires deeper changes and will be added later.
+    :param votes: tensor of teacher votes (bs, n_labels)
+    :param preds: student predictions (bs, n_labels)
+    :return:
+    """
+    bs_range = list(range(votes.size()[0]))
+    ret_vals = [self.gn_max(votes[idx], preds[idx]) for idx in bs_range]
+    indices = [idx for idx in bs_range if ret_vals[idx] is not None]
+    released_vals = ret_vals[indices]
+    return released_vals, indices
+
+  def _data_dependent_rdp(self):
+    """
+    computes and saves the data-dependent epsilon
+    :return:
+    """
     if self.data_dependent_eps is None:
       eps, order = rdp_to_dp(self.orders, self.rdp_eps_by_order, self.target_delta)
       self.selected_order = order
       self.data_dependent_eps = eps
-    return eps
-
-  def check_budget(self):
-    """
-    since the data-dependent epsilon should probably not be used in the algorithm
-    (as then the algorithm contains a secret), no stopping condition based on privacy budget is possible.
-    """
-    # eps, order = rdp_to_dp(self.orders, self.rdp_eps_by_order, self.budget_delta)
-    # if eps > self.budget_eps:
-    #   raise ValueError
-    pass
-
-  # def rdp_max_vote_batch(self, counts, preds):
-  #   """
-  #       depending on threshold_mode, return index of noisy teacher consensus, confident student label or None
-  #       this one assumes batches of counts and preds
-  #       :param counts: teacher votes (bs, n_labels)
-  #       :param preds: student predictions (bs, n_labels)
-  #       :return:
-  #       """
-  #   if self.threshold_mode is 'conf':
-  #     self.add_rdp_loss(counts, self.sigma_thresh)
-  #     if pt.max(counts) + pt.normal(0., self.sigma_thresh) >= self.threshold_T:
-  #       return None
-  #
-  #   elif self.threshold_mode is 'int':
-  #     self.add_rdp_loss(counts, self.sigma_thresh)  # TODO verify this is correct. very likely different.
-  #     if pt.max(counts - self.n_teachers * preds) + pt.normal(0., self.sigma_thresh) >= self.threshold_T:
-  #       pass
-  #     elif pt.max(preds) > self.threshold_gamma:
-  #       # self.check_budget()
-  #       return pt.argmax(preds)
-  #     else:
-  #       return None
-  #
-  #   self.add_rdp_loss(counts, self.sigma_vote)
-  #   # self.check_budget()
-  #   return pt.argmax(counts + pt.normal(pt.ones_like(counts), self.sigma_thresh))
-
-  def choose_beta(self):
-    """
-    this hyperparameter may warrant some search. for now, we just use 0.4/order as recommended in the paper
-    :return: a single beta (may be extended to a list of canididate betas later, with some other changes)
-    """
-    return 0.4/self.selected_order
+    return self.data_dependent_eps
 
   def release_epsilon_fixed_order(self):
     """
-    goes through the vote_log and computes the smooth sensitivity of the data-dependent epsilon
+    goes through the votes_log and computes the smooth sensitivity of the data-dependent epsilon
     depends on the chosen threshold mode.
     As in the Papernot script (smooth_sensitivity_table.py), the best order from the data-dependent RDP cycle is used.
     searching over orders in both this and the data-dependent privacy analysis is very costly,
@@ -202,29 +138,24 @@ class PATEPyTorch:
     :return: parameters of the private epsilon distribution along with a sinlge draw for release.
     """
     order = self.selected_order
-    data_dependent_eps = self.data_dependent_delta_dp()
+    data_dependent_eps = self._data_dependent_rdp()
     ls_by_dist_acc = np.zeros(self.n_teachers)
 
-    for idx, (vote, released) in enumerate(self.vote_log):
-      if self.threshold_mode is not None:
+    for idx, (votes, thresh_votes, released) in enumerate(self.votes_log):
+      if self.threshold_mode is not self.basic_mode:
         # add threshold cost
-        ls_by_dist = local_sensitivity(vote, self.n_teachers, self.sigma_thresh, order, self.threshold_T)
-        ls_by_dist_acc += ls_by_dist
+        ls_by_dist_acc += local_sensitivity(thresh_votes, self.n_teachers, self.sigma_thresh, order, self.threshold_T)
 
       if released:
         # add release cost
-        ls_by_dist = local_sensitivity(vote, self.n_teachers, self.sigma_vote, order)
-        ls_by_dist_acc += ls_by_dist
+        ls_by_dist_acc += local_sensitivity(votes, self.n_teachers, self.sigma_votes, order)
 
-      # compute smooth sensitivity by distounting each
-      beta = self.choose_beta()
-
+      beta = 0.4 / self.selected_order  # for now, we just use the value recommended in the paper
       smooth_s = local_to_smooth_sens(beta, ls_by_dist_acc)
       eps_release_rdp = rdp_eps_release(beta, self.sigma_eps_release, order)
 
       release_mean = data_dependent_eps + eps_release_rdp
       release_sdev = smooth_s * self.sigma_eps_release
-
       release_sample = np.random.normal(release_mean, release_sdev)
 
       return release_sample, release_mean, release_sdev
